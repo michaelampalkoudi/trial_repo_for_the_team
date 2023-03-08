15d434f93c00910b6d2bcc3d9646170fee0938b2
kostas chatz <konschat15@gmail.com>
2023-03-08 10:32:42 +0200

    feat(PTCR-3) some dummy variables

--- hello.py
+++ hello.py
@@ -5,3 +5,17 @@ print('this is a stupid futile try')
 print('to add my git code')
 
 print('to our playground')
+
+# import required modules
+import pandas as pd
+import numpy as np
+
+# create dataset
+df = pd.DataFrame({'Temperature': ['Hot', 'Cold', 'Warm', 'Cold'],
+				})
+
+# display dataset
+print(df)
+
+# create dummy variables
+pd.get_dummies(df)
